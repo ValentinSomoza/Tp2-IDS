a948651124a13f33eae9a1bab39eec04e6bac843
alvaro <aavalos@fi.uba.ar>
2025-09-12 19:31:49 -0300

    Agrego el archivo bash

---
+++ EjPractico2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir TP2-IDS
+cd TP2-IDS
+mkdir .venv
+mkdir static
+cd static
+mkdir css
+mkdir images
+cd ..
+mkdir templates
+mkdir app.py
+pipenv install flask
+pipenv shell
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+
+
