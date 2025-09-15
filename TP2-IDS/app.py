a892de0996bd949075a605092811e219c3267cbb
Valentin <vsomoza@fi.uba.ar>
2025-09-15 18:45:17 -0300

    Acomode informacion y agregue iconos

--- TP2-IDS/app.py
+++ TP2-IDS/app.py
@@ -9,3 +9,17 @@ def index():
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port="5000", debug=True)
+
+    info_evento = {
+1:  { "nombre": "Rally MTB 2025",
+        "organizador": "Club Social y Deportivo Unidos por el Deporte",
+        "descripcion": "Carrera de MTB rural en dos modalidades 30km y 80km ...",
+        "fecha": "24 de Octubre de 2025",
+        "horario": "8am",
+        "lugar": "Tandil, Buenos Aires",
+        "tipo_carrera": "MTB rural",
+        "modalidad_costo": {1: {"nombre": "Corta" ,"valor": "100"},
+                            2: {"nombre": "Larga" ,"valor": "200"}},
+        "Auspiciantes": ["ausp1","auspN"]
+    }
+}
